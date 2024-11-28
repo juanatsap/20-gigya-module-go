c6bbb97c01f586348e944c2bcb39e91dd7097a71
juanatsap <juanatsap@gmail.com>
2024-11-28 08:38:02 +0000

    testing deletion for IdxImport

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -7,17 +7,40 @@ import (
 
 // Account representa una cuenta individual en Gigya
 type Account struct {
-	UID                  string                  `json:"UID"`
-	Profile              Profile                 `json:"profile"`
-	Data                 Data                    `json:"data"`
-	Preferences          Preferences             `json:"preferences"`
-	Subscriptions        map[string]Subscription `json:"subscriptions"`
-	Created              string                  `json:"created"`
-	CreatedTimestamp     int64                   `json:"createdTimestamp"`
-	LastUpdated          string                  `json:"lastUpdated"`
-	LastUpdatedTimestamp int64                   `json:"lastUpdatedTimestamp"`
-	HasLiteAccount       bool                    `json:"hasLiteAccount"`
-	HasFullAccount       bool                    `json:"hasFullAccount"`
+	UID                  string                  `json:"UID,omitempty"`
+	Profile              Profile                 `json:"profile,omitempty"`
+	Data                 Data                    `json:"data,omitempty"`
+	Preferences          Preferences             `json:"preferences,omitempty"`
+	Subscriptions        map[string]Subscription `json:"subscriptions,omitempty"`
+	Created              string                  `json:"created,omitempty"`
+	CreatedTimestamp     int64                   `json:"createdTimestamp,omitempty"`
+	LastUpdated          string                  `json:"lastUpdated,omitempty"`
+	LastUpdatedTimestamp int64                   `json:"lastUpdatedTimestamp,omitempty"`
+	HasLiteAccount       bool                    `json:"hasLiteAccount,omitempty"`
+	HasFullAccount       bool                    `json:"hasFullAccount,omitempty"`
+	Emails               Emails                  `json:"emails,omitempty"`
+	LoginIDs             LoginIDs                `json:"loginIDs,omitempty"`
+	IsVerified           bool                    `json:"isVerified,omitempty"`
+	IsRegistered         bool                    `json:"isRegistered,omitempty"`
+	Password             Password                `json:"password,omitempty"`
+}
+type Password struct {
+	Created        string       `json:"created,omitempty"`
+	HashedPassword string       `json:"hashedPassword,omitempty"`
+	HashSettings   HashSettings `json:"hashSettings,omitempty"`
+}
+type HashSettings struct {
+	Algorithm string `json:"algorithm,omitempty"`
+	Rounds    int    `json:"rounds,omitempty"`
+	Salt      string `json:"salt,omitempty"`
+}
+type Emails struct {
+	Verified   []string `json:"verified,omitempty"`
+	Unverified []string `json:"unverified,omitempty"`
+}
+
+type LoginIDs struct {
+	Emails []string `json:"emails,omitempty"`
 }
 
 func (a Account) Print() {
@@ -34,58 +57,81 @@ func (a Account) Print() {
 	fmt.Printf("  Country: %s\n", a.Profile.Country)
 	fmt.Printf("  Email: %s\n", a.Profile.Email)
 	fmt.Println("")
-	fmt.Println("--------------------")
-	fmt.Println("Data:")
-	fmt.Println("--------------------")
-	// fmt.Printf("  LIVX Member: %s\n", a.Data.LIVXMember)
-	// fmt.Printf("  Visited: %s\n", a.Data.Visited)
-	// fmt.Printf("  Competition: %s\n", *a.Data.Competition.Name)
-	// fmt.Printf("  Favorite Team: %s\n", *a.Data.FavoriteTeam.Name)
-	// fmt.Printf("  Data Source: %s\n", a.Data.DataSource)
-	fmt.Printf("  Events: %s\n", a.Data.Events)
-	fmt.Println("")
+	if a.Data.Competition != nil || a.Data.FavoriteTeam != nil || a.Data.Visited != "" || a.Data.Events != nil {
+
+		fmt.Println("--------------------")
+		fmt.Println("Data:")
+		fmt.Println("--------------------")
+		// fmt.Printf("  LIVX Member: %s\n", a.Data.LIVXMember)
+		// fmt.Printf("  Visited: %s\n", a.Data.Visited)
+		// fmt.Printf("  Competition: %s\n", *a.Data.Competition.Name)
+		// fmt.Printf("  Favorite Team: %s\n", *a.Data.FavoriteTeam.Name)
+		// fmt.Printf("  Data Source: %s\n", a.Data.DataSource)
+		fmt.Printf("  Events: %s\n", a.Data.Events)
+		fmt.Println("")
+	}
 	fmt.Println("--------------------")
 	fmt.Println("Preferences:")
 	fmt.Println("--------------------")
-	fmt.Printf("  Marketing: %v\n", a.Preferences.Marketing)
-	fmt.Printf("  Terms: %v\n", a.Preferences.Terms)
-	fmt.Printf("  Privacy: %v\n", a.Preferences.Privacy)
+	fmt.Printf("  Marketing: %v\n", a.Preferences.Marketing.Email.IsConsentGranted)
+	fmt.Printf("  Terms: %v\n", a.Preferences.Terms.ToS.IsConsentGranted)
+	fmt.Printf("  Privacy: %v\n", a.Preferences.Privacy.Livgolf.IsConsentGranted)
 	fmt.Println("")
-	fmt.Println("--------------------")
-	fmt.Println("Subscriptions:")
-	fmt.Println("--------------------")
-	for channel, subscription := range a.Subscriptions {
-		fmt.Printf("  %s: %v\n", channel, subscription)
-	}
-
 	fmt.Printf("Created: %s\n", a.Created)
 	fmt.Printf("Last Updated: %s\n", a.LastUpdated)
 	fmt.Printf("Has Lite Account: %t\n", a.HasLiteAccount)
 	fmt.Printf("Has Full Account: %t\n", a.HasFullAccount)
 	fmt.Printf("Last Updated Timestamp: %d\n", a.LastUpdatedTimestamp)
 	fmt.Printf("Created Timestamp: %d\n", a.CreatedTimestamp)
+}
 
+func (a Account) PrintShort() {
+	fmt.Println("--------------------")
+	fmt.Println("Profile:")
+	fmt.Println("--------------------")
+	fmt.Printf("  Account UID: %s\n", a.UID)
+	fmt.Printf("  First Name: %s\n", a.Profile.FirstName)
+	fmt.Printf("  Last Name: %s\n", a.Profile.LastName)
+	fmt.Printf("  Country: %s\n", a.Profile.Country)
+	fmt.Printf("  Email: %s\n", a.Profile.Email)
+	fmt.Printf("  Marketing (OPT-IN): %v\n", a.Preferences.Marketing.Email.IsConsentGranted)
+	fmt.Printf("  Terms: %v\n", a.Preferences.Terms.ToS.IsConsentGranted)
+	fmt.Printf("  Privacy: %v\n", a.Preferences.Privacy.Livgolf.IsConsentGranted)
+	fmt.Printf("  Created: %s\n", a.Created)
+	fmt.Printf("  IdxImportID: %s\n", a.Data.IdxImportId)
+	fmt.Println("---------------------------------------------------")
 }
 
 // Preferences representa las preferencias de la cuenta
 type Preferences struct {
-	Marketing map[string]ConsentDetail `json:"marketing"`
-	Terms     map[string]ConsentDetail `json:"terms"`
-	Privacy   map[string]ConsentDetail `json:"privacy"`
+	Marketing Marketing `json:"marketing,omitempty"`
+	Terms     ToS       `json:"terms,omitempty"`
+	Privacy   Livgolf   `json:"privacy,omitempty"`
+}
+
+// Livgolf representa las preferencias de la cuenta
+type Livgolf struct {
+	Livgolf ConsentDetail `json:"livgolf,omitempty"`
+}
+type ToS struct {
+	ToS ConsentDetail `json:"ToS,omitempty"`
+}
+type Marketing struct {
+	Email ConsentDetail `json:"email,omitempty"`
 }
 
 // ConsentDetail representa los detalles de consentimiento
 type ConsentDetail struct {
-	Entitlements        []string                `json:"entitlements"`
-	Locales             map[string]LocaleDetail `json:"locales"`
-	IsConsentGranted    bool                    `json:"isConsentGranted"`
-	ActionTimestamp     string                  `json:"actionTimestamp"`
-	CustomData          []string                `json:"customData"`
-	Language            string                  `json:"language"`
-	LastConsentModified string                  `json:"lastConsentModified"`
+	Entitlements        []string                `json:"entitlements,omitempty"`
+	Locales             map[string]LocaleDetail `json:"locales,omitempty"`
+	IsConsentGranted    bool                    `json:"isConsentGranted,omitempty"`
+	ActionTimestamp     string                  `json:"actionTimestamp,omitempty"`
+	CustomData          []string                `json:"customData,omitempty"`
+	Language            string                  `json:"language,omitempty"`
+	LastConsentModified string                  `json:"lastConsentModified,omitempty"`
 	DocVersion          float64                 `json:"docVersion,omitempty"`
 	DocDate             string                  `json:"docDate,omitempty"`
-	Tags                []string                `json:"tags"`
+	Tags                []string                `json:"tags,omitempty"`
 }
 
 // LocaleDetail representa los detalles específicos de una localidad
@@ -96,23 +142,24 @@ type LocaleDetail struct {
 
 // Subscription representa las suscripciones de la cuenta
 type Subscription struct {
-	Email SubscriptionChannel `json:"email"`
+	Email SubscriptionChannel `json:"email,omitempty"`
 }
 
 // SubscriptionChannel representa los detalles de una suscripción por canal
 type SubscriptionChannel struct {
-	IsSubscribed                 bool              `json:"isSubscribed"`
-	LastUpdatedSubscriptionState string            `json:"lastUpdatedSubscriptionState"`
-	DoubleOptIn                  DoubleOptInDetail `json:"doubleOptIn"`
+	IsSubscribed                 bool              `json:"isSubscribed,omitempty"`
+	LastUpdatedSubscriptionState string            `json:"lastUpdatedSubscriptionState,omitempty"`
+	DoubleOptIn                  DoubleOptInDetail `json:"doubleOptIn,omitempty"`
 }
 
 // DoubleOptInDetail representa los detalles de doble opt-in
 type DoubleOptInDetail struct {
-	Status string `json:"status"`
+	Status string `json:"status,omitempty"`
 }
 
 // Data representa los datos personalizados de la cuenta
 type Data struct {
+	IdxImportId string `json:"idxImportId,omitempty"`
 
 	/* ╭──────────────────────────────────────────╮ */
 	/* │                 LIVGOLF                  │ */
@@ -166,11 +213,15 @@ type Profile struct {
 	LastName  string `json:"lastName,omitempty"`
 	Email     string `json:"email,omitempty"`
 	Country   string `json:"country,omitempty"`
-	ZipCode   string `json:"zipCode,omitempty"`
+	Zip       string `json:"zip,omitempty"`
 	City      string `json:"city,omitempty"`
 	State     string `json:"state,omitempty"`
 }
 
+func (a Account) AsJSON() string {
+	data, _ := json.Marshal(a)
+	return string(data)
+}
 func (a Profile) AsJSON() string {
 	data, _ := json.Marshal(a)
 	return string(data)
--- accounts/accountsAPI.go
+++ accounts/accountsAPI.go
@@ -138,6 +138,7 @@ func (a *AccountsAPI) GetAccountInfo(UID string) (Account, error) {
 		"apiKey":  a.apiKey,
 		"userKey": a.userKey,
 		"secret":  a.secretKey,
+		"include": "profile, data, preferences",
 	}
 
 	// Preparar la URL de la solicitud
@@ -172,16 +173,42 @@ func (a *AccountsAPI) GetAccountInfo(UID string) (Account, error) {
 		return Account{}, fmt.Errorf("API error %d: %s", response.ErrorCode, response.StatusReason)
 	}
 
-	return Account{UID: response.UID}, nil
+	// Create a new Account object
+	account := Account{
+		UID:     response.UID,
+		Profile: response.Profile,
+		Data:    response.Data,
+		Preferences: Preferences{
+			Marketing: Marketing{
+				Email: ConsentDetail{
+					IsConsentGranted: response.Preferences.Marketing.Email.IsConsentGranted,
+				},
+			},
+			Terms: ToS{
+				ToS: ConsentDetail{
+					IsConsentGranted: response.Preferences.Terms.ToS.IsConsentGranted,
+				},
+			},
+			Privacy: Livgolf{
+				Livgolf: ConsentDetail{
+					IsConsentGranted: response.Preferences.Privacy.Livgolf.IsConsentGranted,
+				},
+			},
+		},
+		Created: response.Created,
+	}
+
+	return account, nil
 }
 func (a *AccountsAPI) ImportFullAccount(account Account) (Account, error) {
-
 	// Añadir parámetros
 	method := "accounts.importFullAccount"
 	params := map[string]string{
-		"apiKey":  a.apiKey,
-		"userKey": a.userKey,
-		"secret":  a.secretKey,
+		"apiKey":       a.apiKey,
+		"userKey":      a.userKey,
+		"secret":       a.secretKey,
+		"importPolicy": "upsert",
+		"account":      account.AsJSON(),
 	}
 
 	// Preparar la URL de la solicitud
@@ -213,15 +240,124 @@ func (a *AccountsAPI) ImportFullAccount(account Account) (Account, error) {
 
 	// Verificar si hubo un error en la respuesta
 	if response.ErrorCode != 0 {
-		return Account{}, fmt.Errorf("API error %d: %s", response.ErrorCode, response.StatusReason)
+		return Account{}, fmt.Errorf("API error %d: %s, %s\n\nDetails: %s", response.ErrorCode, response.StatusReason, response.ErrorMessage, response.ErrorDetails)
 	}
 
 	return Account{UID: response.UID}, nil
 }
+func (a *AccountsAPI) DeleteAccount(UID string) (Account, error) {
 
+	// Añadir parámetros
+	method := "accounts.deleteAccount"
+	params := map[string]string{
+		"UID":     UID,
+		"apiKey":  a.apiKey,
+		"userKey": a.userKey,
+		"secret":  a.secretKey,
+	}
+
+	// Preparar la URL de la solicitud
+	baseURL := fmt.Sprintf("https://%s/%s", a.apiDomain, method)
+	data := url.Values{}
+	for key, value := range params {
+		data.Set(key, value)
+	}
+
+	// Enviar la solicitud POST
+	resp, err := http.Post(baseURL, "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
+	if err != nil {
+		return Account{}, err
+	}
+	defer resp.Body.Close()
+
+	// Leer la respuesta
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return Account{}, err
+	}
+
+	// Deserializar la respuesta JSON en SearchResponse
+	var response DeleteAccountResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return Account{}, err
+	}
+
+	// Verificar si hubo un error en la respuesta
+	if response.ErrorCode != 0 {
+		return Account{}, fmt.Errorf("API error %d: %s, %s\n\nDetails: %s", response.ErrorCode, response.StatusReason, response.ErrorMessage, response.ErrorDetails)
+	}
+
+	return Account{UID: response.UID}, nil
+}
+func (a *AccountsAPI) SearchAccountsForIdxImportId(idxImportId string) ([]Account, error) {
+
+	query := fmt.Sprintf("Select * from accounts where idxImportId='%s'", idxImportId)
+	// Añadir parámetros
+	method := "accounts.search"
+	params := map[string]string{
+		"apiKey":  a.apiKey,
+		"userKey": a.userKey,
+		"secret":  a.secretKey,
+		"query":   query,
+	}
+
+	// Preparar la URL de la solicitud
+	baseURL := fmt.Sprintf("https://%s/%s", a.apiDomain, method)
+	data := url.Values{}
+	for key, value := range params {
+		data.Set(key, value)
+	}
+
+	// Enviar la solicitud POST
+	resp, err := http.Post(baseURL, "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
+	if err != nil {
+		return []Account{}, err
+	}
+	defer resp.Body.Close()
+
+	// Leer la respuesta
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return []Account{}, err
+	}
+
+	// Deserializar la respuesta JSON en SearchResponse
+	var response SearchResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return []Account{}, err
+	}
+
+	// Verificar si hubo un error en la respuesta
+	if response.ErrorCode != 0 {
+		return []Account{}, fmt.Errorf("API error %d: %s\n\nDetails: %s", response.ErrorCode, response.StatusReason, response.ErrorDetails)
+	}
+
+	return response.Results, nil
+}
+func (a *AccountsAPI) DeleteAccountsForIdxImportId(idxImportId string) ([]Account, error) {
+	accounts, err := a.SearchAccountsForIdxImportId(idxImportId)
+	if err != nil {
+		return []Account{}, err
+	}
+	for _, account := range accounts {
+		_, err := a.DeleteAccount(account.UID)
+		if err != nil {
+			return []Account{}, err
+		}
+	}
+	return accounts, nil
+}
+
+/* ╭──────────────────────────────────────────╮ */
+/* │                RESPONSES                 │ */
+/* ╰──────────────────────────────────────────╯ */
 type ImportFullAccountResponse struct {
 	CallID       string `json:"callId"`
 	ErrorCode    int    `json:"errorCode"`
+	ErrorMessage string `json:"errorMessage"`
+	ErrorDetails string `json:"errorDetails"`
 	APIVersion   int    `json:"apiVersion"`
 	StatusCode   int    `json:"statusCode"`
 	StatusReason string `json:"statusReason"`
@@ -233,6 +369,7 @@ type ImportFullAccountResponse struct {
 type SearchResponse struct {
 	CallID       string   `json:"callId"`
 	ErrorCode    int      `json:"errorCode"`
+	ErrorDetails string   `json:"errorDetails"`
 	APIVersion   int      `json:"apiVersion"`
 	StatusCode   int      `json:"statusCode"`
 	StatusReason string   `json:"statusReason"`
@@ -273,16 +410,39 @@ type SetAccountInfoResponse struct {
 	UID          string `json:"UID"`
 }
 type GetAccountInfoResponse struct {
-	CallID       string      `json:"callId"`
-	ErrorCode    int         `json:"errorCode"`
-	APIVersion   int         `json:"apiVersion"`
-	StatusCode   int         `json:"statusCode"`
-	StatusReason string      `json:"statusReason"`
-	Time         string      `json:"time"`
-	UID          string      `json:"UID"`
-	Profile      Profile     `json:"profile"`
-	Data         Data        `json:"data"`
-	Preferences  Preferences `json:"preferences"`
+	CallID               string                  `json:"callId"`
+	ErrorCode            int                     `json:"errorCode"`
+	APIVersion           int                     `json:"apiVersion"`
+	StatusCode           int                     `json:"statusCode"`
+	StatusReason         string                  `json:"statusReason"`
+	Time                 string                  `json:"time"`
+	UID                  string                  `json:"UID"`
+	Profile              Profile                 `json:"profile,omitempty"`
+	Data                 Data                    `json:"data,omitempty"`
+	Preferences          Preferences             `json:"preferences,omitempty"`
+	Subscriptions        map[string]Subscription `json:"subscriptions,omitempty"`
+	Created              string                  `json:"created,omitempty"`
+	CreatedTimestamp     int64                   `json:"createdTimestamp,omitempty"`
+	LastUpdated          string                  `json:"lastUpdated,omitempty"`
+	LastUpdatedTimestamp int64                   `json:"lastUpdatedTimestamp,omitempty"`
+	HasLiteAccount       bool                    `json:"hasLiteAccount,omitempty"`
+	HasFullAccount       bool                    `json:"hasFullAccount,omitempty"`
+	Emails               Emails                  `json:"emails,omitempty"`
+	LoginIDs             LoginIDs                `json:"loginIDs,omitempty"`
+	IsVerified           bool                    `json:"isVerified,omitempty"`
+	IsRegistered         bool                    `json:"isRegistered,omitempty"`
+	Password             Password                `json:"password,omitempty"`
+}
+type DeleteAccountResponse struct {
+	CallID       string `json:"callId"`
+	ErrorCode    int    `json:"errorCode"`
+	ErrorMessage string `json:"errorMessage"`
+	ErrorDetails string `json:"errorDetails"`
+	APIVersion   int    `json:"apiVersion"`
+	StatusCode   int    `json:"statusCode"`
+	StatusReason string `json:"statusReason"`
+	Time         string `json:"time"`
+	UID          string `json:"UID"`
 }
 type GroupedLIVGolfItems []GroupedLIVGolfItem
 
