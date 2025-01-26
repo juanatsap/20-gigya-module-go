5448175b17cb4af5a6e3a2d29e9e610122e4372c
juanatsap <juanatsap@gmail.com>
2025-01-26 21:34:51 +0000

    bdfore bd

--- extensions/claims.go
+++ extensions/claims.go
@@ -20,7 +20,7 @@ func MapClaimsToStruct(claims jwt.MapClaims, target interface{}) error {
 /*
 jwt.Claims(github.com/golang-jwt/jwt.MapClaims) ["apiKey": "4_fok0Wsjf2RMSy-Oksfktjw", "callID": "3aa65ea84e7c422f8238e08ad64b81e6", "extensionPoint": "OnBeforeAccountsLogin", "data": map[string]interface {} ["params": *(*interface {})(0x14000000628), "accountInfo": *(*interface {})(0x14000000638), "context": *(*interface {})(0x14000000648), ], ]
 */
-type LoginClaims struct {
+type ExtensionClaims struct {
 	ApiKey         string `json:"apiKey"`
 	CallID         string `json:"callID"`
 	ExtensionPoint string `json:"extensionPoint"`
